1ea0538c4808bf6e7c82951c4aa8f06af09e4dbc
Dhruv Bansal <dhruvbansal0612@gmail.com>
2025-05-29 16:54:54 +0530

    new change

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -14,7 +14,7 @@ const mailSender = async(email, title, body ) =>{
         })
 
         let info = await transporter.sendMail({
-            from : 'StudyNotion',
+            from : 'Edufy',
             to: `${email}`,
             subject: `${title}`,
             html: `${body}`,
